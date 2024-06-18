7dbeed944e76d2b5b00ca77963668d2a9b46649f
Eric Araújo Lima <eric.araujolima@hotmail.com>
2024-06-17 21:58:42 -0300

    Criando Projeto

---
+++ src/Model/Curso.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Alura\Solid\Model;
+
+class Curso
+{
+    private string $nome;
+    private array $videos;
+    /**
+     * @var  Feedback[] $feedbacks
+     */
+    private array $feedbacks;
+
+    public function __construct(string $nome)
+    {
+        $this->nome = $nome;
+        $this->videos = [];
+        $this->feedbacks = [];
+    }
+
+    public function receberFeedback(Feedback $feedback): void
+    {
+        $this->feedbacks[] = $feedback;
+    }
+
+    public function adicionarVideo(Video $video): void
+    {
+        if ($video->minutosDeDuracao() < 3) {
+            throw new \DomainException('Video muito curto');
+        }
+
+        $this->videos[] = $video;
+    }
+
+    /** @return Video[] */
+    public function recuperarVideos(): array
+    {
+        return $this->videos;
+    }
+}
